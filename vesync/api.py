55065f5a254615204931b41f4980c20b84c9ac7b
(Kristjan) Eerik Kaseniit <eerik@counsyl.com>
2018-01-31 23:39:13 -0800

    heavier caching and flake8

--- vesync/api.py
+++ vesync/api.py
@@ -7,7 +7,7 @@ requests.packages.urllib3.disable_warnings()
 BASE_URL = "https://smartapi.vesync.com"
 
 
-class VesyncApi:
+class VesyncApi(object):
     def __init__(self, username, password):
         payload = json.dumps({
             "account": username,
@@ -20,29 +20,36 @@ class VesyncApi:
         else:
             self._account = account
         self._devices = []
+        self._name2id = {}
+        self._headers = {'tk': self._account["tk"], 'accountid': self._account["accountID"]}
 
-    def get_devices(self):
-        self._devices = requests.get(BASE_URL + '/vold/user/devices', verify=False, headers=self.get_headers()).json()
+    def get_devices(self, force_update=False):
+        if force_update or not self._devices:
+            print("# GRABBING DEVICES")
+            self._devices = requests.get(BASE_URL + '/vold/user/devices', verify=False,
+                                         headers=self._headers).json()
         return self._devices
 
     def turn_on(self, device_id):
-        requests.put(BASE_URL + '/v1/wifi-switch-1.3/' + device_id + '/status/on', verify=False, data={}, headers=self.get_headers())
+        requests.put(BASE_URL + '/v1/wifi-switch-1.3/' + device_id + '/status/on', verify=False,
+                     data={}, headers=self._headers)
 
     def turn_off(self, device_id):
-        requests.put(BASE_URL + '/v1/wifi-switch-1.3/' + device_id + '/status/off', verify=False, data={}, headers=self.get_headers())
+        requests.put(BASE_URL + '/v1/wifi-switch-1.3/' + device_id + '/status/off', verify=False,
+                     data={}, headers=self._headers)
 
-    def turn_on_by_name(self, device_name):
+    def turn_on_by_name(self, device_name, force_update=False):
         self.turn_on(self.name2id(device_name))
 
-    def turn_off_by_name(self, device_name):
+    def turn_off_by_name(self, device_name, force_update=False):
         self.turn_off(self.name2id(device_name))
 
-    def name2id(self, device_name):
-        for device in self.get_devices():
-            if device['deviceName'] == device_name:
-                return device['cid']
-        raise ValueError('Unknown device name')
-
-    def get_headers(self):
-        return {'tk': self._account["tk"],
-                'accountid': self._account["accountID"]}
+    def name2id(self, device_name, force_update=False):
+        if force_update or device_name not in self._name2id:
+            for device in self.get_devices():
+                if device['deviceName'] == device_name:
+                    self._name2id[device_name] = device['cid']
+                    break
+            else:
+                raise ValueError('Unknown device name')
+        return self._name2id[device_name]
